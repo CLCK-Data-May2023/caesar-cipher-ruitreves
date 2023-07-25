6a40337b81395fcffa27f13b2f5ff8d79cc43993
Rui Treves <trevesrui@gmail.com>
2023-07-25 12:17:06 -0400

    REAL ACTUALSOLUTION

--- cipher.py
+++ cipher.py
@@ -1,8 +1,8 @@
-# add your code here
 ab = ["a", "b", "c", "d", "e", "f", "g", "h" ,"i" ,"j", 
       "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
       "u", "v", "w", "x", "y", "z"]
-sentence = input("enter a sentence: ")
+sentence = input("Please enter a sentence: ")
+sentence = sentence.lower()
 
 code_words = sentence.split()
 
@@ -10,17 +10,9 @@ code_words = sentence.split()
 code_letters = ""
 for word in code_words:
     for letter in word:
-        code_letters += ab[(ab.index(letter) + 5) % 26]
+        if letter in ab:
+            code_letters += ab[(ab.index(letter) + 5) % 26]
+        else: 
+            code_letters += letter
     code_letters += " "
-print(code_letters) 
-
-#undo code
-#code_sentence = input("Enter coded sentence: ")
-#uncode_word = code_sentence.split()
-
-#uncode_letters = ''
-#for word in uncode_word:
-    #for letter in word:
-        #uncode_letters += ab[(ab.index(letter) - 5) % 26]
-    #uncode_letters += " "
-#print(uncode_letters)
+print("The encrypted sentence is:", code_letters) 
